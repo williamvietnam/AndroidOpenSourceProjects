82873666abd419e5df7b185ecba4aa0d3963cf02
NguyenBangGiang <williamnb.vn@gmail.com>
2023-02-08 20:37:36 +0700

    android-common-usecase: navigate use case from main screen

--- AndroidCommonUseCases/AndroidCommonUseCasesSourceCode/app/src/main/java/com/android/login_form_use_case/LoginFormFragment.java
+++ AndroidCommonUseCases/AndroidCommonUseCasesSourceCode/app/src/main/java/com/android/login_form_use_case/LoginFormFragment.java
@@ -1,4 +1,29 @@
 package com.android.login_form_use_case;
 
-public class LoginFormFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.databinding.LoginFormBinding;
+
+public class LoginFormFragment extends Fragment {
+
+    private LoginFormBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.binding = LoginFormBinding.inflate(inflater, container, false);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
