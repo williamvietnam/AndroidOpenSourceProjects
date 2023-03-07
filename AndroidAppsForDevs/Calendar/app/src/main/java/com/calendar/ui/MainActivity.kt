28c6ea59b877437156c2976205bf8a31ff57dd07
NguyenBangGiang <williamnb.vn@gmail.com>
2023-03-07 11:18:26 +0700

    Calendar: material design calendar

--- AndroidAppsForDevs/Calendar/app/src/main/java/com/calendar/ui/MainActivity.kt
+++ AndroidAppsForDevs/Calendar/app/src/main/java/com/calendar/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.calendar
+package com.calendar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.calendar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
