fa5dcb414a2e4fdfec9faa495efea7b91374aa13
NguyenBangGiang <williamnb.vn@gmail.com>
2023-03-19 17:51:50 +0700

    MultiApplication: Container multi-apps

---
+++ MultiApplicationProject/app/src/main/java/com/multiapplication/container/App.kt
@@ -0,0 +1,6 @@
+package com.multiapplication.container
+
+data class App(
+    val logo: Int,
+    val name: String,
+)
---
+++ MultiApplicationProject/app/src/main/java/com/multiapplication/container/AppsAdapter.kt
@@ -0,0 +1,4 @@
+package com.multiapplication.container
+
+class AppsAdapter {
+}
