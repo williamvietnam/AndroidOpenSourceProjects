6e990b60c0fb47dc7243905ddc35bd68c885d71f
ducbeo38 <agger6880@gmail.com>
2023-06-16 14:51:55 +0700

    remoteControl/sony: SonyIRCC

---
+++ Remote/src/main/java/com/remote/brands/sony/api/SonyIRCC.kt
@@ -0,0 +1,63 @@
+package com.remote.brands.sony.api
+
+object SonyIRCC {
+    const val Power = "AAAAAQAAAAEAAAAVAw=="
+
+    const val Input = "AAAAAQAAAAEAAAAlAw=="
+
+    const val SyncMenu = "AAAAAgAAABoAAABYAw=="
+
+    const val Num1 = "AAAAAQAAAAEAAAAAAw=="
+
+    const val Num2 = "AAAAAQAAAAEAAAABAw=="
+
+    const val Num3 = "AAAAAQAAAAEAAAACAw=="
+
+    const val Num4 = "AAAAAQAAAAEAAAADAw=="
+
+    const val Num5 = "AAAAAQAAAAEAAAAEAw=="
+
+    const val Num6 = "AAAAAQAAAAEAAAAFAw=="
+
+    const val Num7 = "AAAAAQAAAAEAAAAGAw=="
+
+    const val Num8 = "AAAAAQAAAAEAAAAHAw=="
+
+    const val Num9 = "AAAAAQAAAAEAAAAIAw=="
+
+    const val Num0 = "AAAAAQAAAAEAAAAJAw=="
+
+    const val Up = "AAAAAQAAAAEAAAB0Aw=="
+
+    const val Down = "AAAAAQAAAAEAAAB1Aw=="
+
+    const val Left = "AAAAAQAAAAEAAAA0Aw=="
+
+    const val Right = "AAAAAQAAAAEAAAAzAw=="
+
+    const val Confirm = "AAAAAQAAAAEAAABlAw=="
+
+    const val Home = "AAAAAQAAAAEAAABgAw=="
+
+    const val VolumeUp = "AAAAAQAAAAEAAAASAw=="
+
+    const val VolumeDown = "AAAAAQAAAAEAAAATAw=="
+
+    const val Mute = "AAAAAQAAAAEAAAAUAw=="
+
+    const val Audio = "AAAAAQAAAAEAAAAXAw=="
+
+    const val ChannelUp = "AAAAAQAAAAEAAAAQAw=="
+
+    const val ChannelDown = "AAAAAQAAAAEAAAARAw=="
+
+    const val Play = "AAAAAgAAAJcAAAAaAw=="
+
+    const val Pause = "AAAAAgAAAJcAAAAZAw=="
+
+    const val Stop = "AAAAAgAAAJcAAAAYAw=="
+
+    const val Prev = "AAAAAgAAAJcAAAA8Aw=="
+
+    const val Next = "AAAAAgAAAJcAAAA9Aw=="
+}
