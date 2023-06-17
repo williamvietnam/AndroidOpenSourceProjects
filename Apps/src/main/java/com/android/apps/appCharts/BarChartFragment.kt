4290fa0805054bd9f8c331ff0f9d231aeac58dad
NguyenBangGiang <williamnb.vn@gmail.com>
2023-06-18 00:11:23 +0700

    Develop/Apps: init charts and shorts app

---
+++ Apps/src/main/java/com/android/apps/appCharts/BarChartFragment.kt
@@ -0,0 +1,28 @@
+package com.android.apps.appCharts
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.android.commons.base.BaseFragment
+import com.android.databinding.FragmentBarChartBinding
+
+class BarChartFragment : BaseFragment<FragmentBarChartBinding, ChartsViewModel>() {
+    override fun createViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentBarChartBinding {
+        return FragmentBarChartBinding.inflate(inflater, container, false)
+    }
+
+    override fun createViewModel(): ChartsViewModel {
+        return ViewModelProvider(this)[ChartsViewModel::class.java]
+    }
+
+    override fun initializeViews() {
+
+    }
+
+    override fun initializeEvents() {
+
+    }
+}
---
+++ Apps/src/main/java/com/android/apps/appVideoShorts/VideoShortsFragment.kt
@@ -0,0 +1,28 @@
+package com.android.apps.appVideoShorts
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.android.commons.base.BaseFragment
+import com.android.databinding.FragmentVideoShortsBinding
+
+class VideoShortsFragment : BaseFragment<FragmentVideoShortsBinding, VideoShortsViewModel>() {
+    override fun createViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentVideoShortsBinding {
+        return FragmentVideoShortsBinding.inflate(inflater, container, false)
+    }
+
+    override fun createViewModel(): VideoShortsViewModel {
+        return ViewModelProvider(this)[VideoShortsViewModel::class.java]
+    }
+
+    override fun initializeViews() {
+
+    }
+
+    override fun initializeEvents() {
+
+    }
+}
