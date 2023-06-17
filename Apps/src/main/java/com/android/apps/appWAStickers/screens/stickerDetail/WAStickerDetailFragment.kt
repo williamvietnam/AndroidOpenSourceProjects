11c0598859101481007bb688c104b0b330f58384
NguyenBangGiang <williamnb.vn@gmail.com>
2023-06-17 21:52:39 +0700

    Apps/waStickers: Sticker Preview Screen

--- Apps/src/main/java/com/android/apps/appWAStickers/screens/stickerDetail/WAStickerDetailFragment.kt
+++ Apps/src/main/java/com/android/apps/appWAStickers/screens/stickerDetail/WAStickerDetailFragment.kt
@@ -1,29 +1,109 @@
 package com.android.apps.appWAStickers.screens.stickerDetail
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.android.commons.base.BaseFragment
+import androidx.viewpager2.widget.ViewPager2
+import com.android.apps.appWAStickers.core.AddStickerPackFragment
+import com.android.apps.appWAStickers.models.StickerPack
+import com.android.commons.utilities.Constants
+import com.android.commons.utilities.Preferences
 import com.android.databinding.FragmentWAStickerDetailBinding
 
-class WAStickerDetailFragment :
-    BaseFragment<FragmentWAStickerDetailBinding, WAStickerDetailViewModel>() {
-    override fun createViewBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentWAStickerDetailBinding {
-        return FragmentWAStickerDetailBinding.inflate(inflater, container, false)
+class WAStickerDetailFragment : AddStickerPackFragment() {
+    private lateinit var binding: FragmentWAStickerDetailBinding
+    private lateinit var viewModel: WAStickerDetailViewModel
+    private lateinit var pagerAdapter: WAStickerPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[WAStickerDetailViewModel::class.java]
     }
 
-    override fun createViewModel(): WAStickerDetailViewModel {
-        return ViewModelProvider(this)[WAStickerDetailViewModel::class.java]
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWAStickerDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun initializeViews() {
-        TODO("Not yet implemented")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.positionStartWhenScreenIsCalled = arguments?.getInt(
+            Constants.STICKER_POSITION
+        ) as Int
+        val data = arguments?.getParcelable(Constants.STICKER_DATA) as? StickerPack
+        if (data != null) {
+            viewModel.receiveData(data)
+        }
+        initializeViews()
+        initializeEvents()
     }
 
-    override fun initializeEvents() {
-        TODO("Not yet implemented")
+    private fun initializeViews() {
+        binding.tvStickerPackageName.text = Preferences.instance.get(
+            Constants.STICKER_PACKAGE_NAME, ""
+        ) as String
+        pagerAdapter = WAStickerPagerAdapter()
+        viewModel.getStickersData.observe(viewLifecycleOwner) {
+            pagerAdapter.loadData(it)
+            viewModel.dataSize = it.size
+        }
+        binding.viewPager.adapter = pagerAdapter
+
+        if (Preferences.instance.get(Constants.STICKER_IS_WHITELISTED, false) as Boolean) {
+            binding.flButton.visibility = View.INVISIBLE
+        } else {
+            binding.flButton.visibility = View.VISIBLE
+        }
+
+        // set position display when click sticker from StickerFragment
+        binding.viewPager.post {
+            binding.viewPager.currentItem = viewModel.positionStartWhenScreenIsCalled
+        }
+    }
+
+    private fun initializeEvents() {
+        binding.buttonClose.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commitNow()
+        }
+
+        binding.buttonPrevious.setOnClickListener {
+            binding.viewPager.currentItem = binding.viewPager.currentItem - 1
+            if (binding.viewPager.currentItem > 0) {
+                binding.viewPager.setCurrentItem(binding.viewPager.currentItem, true)
+            }
+        }
+
+        binding.buttonNext.setOnClickListener {
+            binding.viewPager.currentItem = binding.viewPager.currentItem + 1
+            if (binding.viewPager.currentItem < (viewModel.dataSize - 1)) {
+                binding.viewPager.setCurrentItem(binding.viewPager.currentItem, true)
+            }
+        }
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.indicator.text = String
+                    .format("${binding.viewPager.currentItem + 1}/${viewModel.dataSize}")
+                if ((position > 0) && (position < (viewModel.dataSize - 1))) {
+                    binding.buttonPrevious.visibility = View.VISIBLE
+                    binding.buttonNext.visibility = View.VISIBLE
+                } else if (position == 0) {
+                    binding.buttonPrevious.visibility = View.GONE
+                } else if (position == (viewModel.dataSize - 1)) {
+                    binding.buttonNext.visibility = View.GONE
+                }
+            }
+        })
+
+        binding.flButton.setOnClickListener {
+            viewModel.getStickerPackData.observe(viewLifecycleOwner) {
+                addStickerPackToWhatsApp(it.identifier!!, it.stickerPackName!!)
+            }
+        }
     }
 }
--- Apps/src/main/java/com/android/apps/appWAStickers/screens/stickerDetail/WAStickerDetailViewModel.kt
+++ Apps/src/main/java/com/android/apps/appWAStickers/screens/stickerDetail/WAStickerDetailViewModel.kt
@@ -1,6 +1,26 @@
 package com.android.apps.appWAStickers.screens.stickerDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.apps.appWAStickers.models.Sticker
+import com.android.apps.appWAStickers.models.StickerPack
 import com.android.commons.base.BaseViewModel
 
 class WAStickerDetailViewModel: BaseViewModel() {
+    private val stickersData = MutableLiveData<ArrayList<Sticker>>().apply {
+        value = ArrayList()
+    }
+    private val stickerPackData = MutableLiveData<StickerPack>().apply {
+        value = StickerPack()
+    }
+    val getStickersData: LiveData<ArrayList<Sticker>> = stickersData
+    val getStickerPackData: LiveData<StickerPack> = stickerPackData
+
+    var positionStartWhenScreenIsCalled: Int = 0
+    var dataSize: Int = 0
+
+    fun receiveData(stickerPack: StickerPack) {
+        stickersData.postValue(stickerPack.stickers as ArrayList<Sticker>?)
+        stickerPackData.postValue(stickerPack)
+    }
 }
