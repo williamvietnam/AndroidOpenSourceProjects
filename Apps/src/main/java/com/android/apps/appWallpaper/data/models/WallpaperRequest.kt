866e01ee7a600049d5fd5c75410a37f897985c8f
ducbeo38 <agger6880@gmail.com>
2023-06-21 17:19:11 +0700

    Apps/AppWallpaper: init project

---
+++ Apps/src/main/java/com/android/apps/appWallpaper/data/models/WallpaperRequest.kt
@@ -0,0 +1,5 @@
+package com.android.apps.appWallpaper.data.models
+
+data class WallpaperRequest<T>(
+    var params: MutableList<T>
+)
---
+++ Apps/src/main/java/com/android/apps/appWallpaper/data/network/WallpaperApiClient.kt
@@ -0,0 +1,4 @@
+package com.android.apps.appWallpaper.data.network
+
+class WallpaperApiClient {
+}
